8da437ed72af410c32102d3385fcc7bf5da4d4ae
Tien Nguyen <mrtienhp97@gmail.com>
2017-12-18 21:51:54 +0700

    asf

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,6 +84,11 @@
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
+
+                @foreach($products as $product)
+                    {{ $product["id"] }}
+                @endforeach
+                
             </div>
         </div>
     </body>
