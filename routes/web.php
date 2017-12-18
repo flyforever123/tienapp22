063904781e1b3e85fda3a35dc66137caed0c7436
Tien Nguyen <mrtienhp97@gmail.com>
2017-12-18 15:08:12 +0700

    dasdas

--- routes/web.php
+++ routes/web.php
@@ -11,22 +11,4 @@
 |
 */
 
-Route::get('/', function () {
-	$shop = ShopifyApp::shop();
-
-    $shopify = App::make('ShopifyAPI', [ 
-        'API_KEY'       => $shop->api()->setApiKey(), 
-        'API_SECRET'    => $shop->api()->setApiSecret(), 
-        'SHOP_DOMAIN'   => $shop->api()->getShop(), 
-        'ACCESS_TOKEN'  => $shop->api()->getAccessToken() 
-    ]);
-
-    // Gets a list of products 
-    $result = $shopify->call([ 
-        'METHOD'    => 'GET', 
-        'URL'       => '/admin/products.json?page=1' 
-    ]); 
-    $products = $result->products; 
-
-    return view('welcome', compact('products'));
-});
+Route::get('/', 'ProductController@index');
